595bafb8aa0a8c760ff198921bc555c9bda77900
khanhduy <duy34282@gmail.com>
2022-07-03 21:35:47 +0700

    update thanhtoan

--- src/pages/Trangchu/test.vue
+++ src/pages/Trangchu/test.vue
@@ -5,129 +5,112 @@
         <div class="container">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="#/giohang">Giỏ hàng</a></li>
-                    <li class="breadcrumb-item"><a href="#">Thông tin giao hàng</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Phương thức thanh toán</li>
+                    <li class="breadcrumb-item"><a href="#">Trang chủ</a></li>
+                    <li class="breadcrumb-item"><a href="#">Sản phẩm</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Chi tiết sản phẩm</li>
                 </ol>
             </nav>
+            <div class="row">
+
+                <h5 class="card-title" style="font-weight: bold">
+                    Laptop Asus TUF Gaming FX506LHB-HN188W i5 10300H/8GB/512GB/15.6"FHD/GTX 1650 4GB/Win11
+                </h5>
+            </div>
+
 
             <div class="row">
                 <!-- left -->
 
-                <div class="col-md-5">
+                <div class="col-md-6">
                     <div class="col-12">
-                        <h5 class="card-title" style="font-weight: bold">
-                            Thông tin giao hàng
-                        </h5>
-                        <span>Bạn đã có tài khoản?&nbsp;<a href="#">Đăng nhập</a></span>
-                        <form>
-                            <div class="form-group">
-                                <label for="inputEmail4">Họ và tên</label>
-                                <input type="text" class="form-control" id="inputAddress2" placeholder="Họ và Tên">
-                            </div>
-                            <div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <label for="inputEmail4">Email</label>
-                                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="inputEmail4">Số điện thoại</label>
-                                    <input type="text" class="form-control" id="inputPassword4"
-                                        placeholder="Số điện thoại">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="inputEmail4">Địa chỉ</label>
-                                <input type="text" class="form-control" id="inputAddress" placeholder="Địa chỉ">
-                            </div>
-                            <button type="submit" class="row-sm-2 btn-primary btn-fill btn btn-lg btn-block"
-                                @click="notifyVue('top', 'right')">Thanh toán</button>
-                        </form>
-                    </div>
-                </div>
-
-
-
-                <!--right-->
-                <div class="col-md-7">
-                    <div class="col-md-12">
-                        <h5 class="card-title" style="font-weight: bold">
-                            &nbsp;
-                        </h5>
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="row">
-                                    <table class="table table-bordered">
-                                        <thead>
-                                            <tr>
-                                                <th class="col-md-2"
-                                                    style="text-align: center !important ; font-weight: bold; color:black">
-                                                    Sản phẩm</th>
-                                                <th class="col-md-5"
-                                                    style="text-align: center !important ; font-weight: bold; color:black">
-                                                    Tên sản phẩm</th>
-                                                <th class="col-md-2"
-                                                    style="text-align: center !important ; font-weight: bold; color:black">
-                                                    Số lượng</th>
-                                                <th class="auto"
-                                                    style="text-align: center !important ; font-weight: bold; color:black">
-                                                    Giá sản phẩm</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <th scope="row"><img class="img-fluid" src="img/faces/laptop-1.jpg"
-                                                        alt="..." /></th>
-                                                <td>Laptop ASUS TUF Gaming F15 FX506LHB HN188W</td>
-                                                <td style="text-align: center !important">3</td>
-                                                <td style="text-align: center !important">45.000.000</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row"><img class="img-fluid" src="img/faces/laptop-2.jpg"
-                                                        alt="..." /></th>
-                                                <td>Laptop ASUS TUF Gaming F15 FX506LHB HN188W</td>
-                                                <td style="text-align: center !important">2</td>
-                                                <td style="text-align: center !important">45.000.000</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row"><img class="img-fluid" src="img/faces/laptop-1.jpg"
-                                                        alt="..." /></th>
-                                                <td>Laptop ASUS TUF Gaming F15 FX506LHB HN188W</td>
-                                                <td style="text-align: center !important">1</td>
-                                                <td>45.000.000</td>
-                                            </tr>
- <tr>
-                                                <td colspan="3">
-                                                    Tạm tính:
-                                                    <br>
-                                                    Phí vận chuyển:
-                                                    
-                                                    </td>
-                                                <td
-                                                    >
-                                                    100.100.000<span>đ</span>
-                                                    
-                                                    </td>
-                                            </tr>
-
-                                            <tr>
-                                                <td colspan="3"
-                                                    style=" font-weight: bold; color:black">
-                                                    Tổng cộng</td>
-                                                <td
-                                                    style="text-align: center !important ; font-weight: bold; color:black">
-                                                    100.100.000<span>đ</span></td>
-                                            </tr>
-                                            <tr>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
+                        <div class="card" style="">
+                            <img class="img-fluid" src="img/faces/laptop-2.jpg" alt="..." />
                         </div>
                     </div>
-
                 </div>
+                <!--right-->
+                <div class="col-md-6">
+                    <div class="col-12">
+                        <!-- <h5 class="card-title" style="font-weight: bold">
+                            &nbsp;
+                        </h5> -->
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <td colspan="2" style="font-weight: bold; text-align: center!important;">Thông số
+                                        chi tiết</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td width="30%">Hãng</td>
+                                    <td>Acer</td>
+                                </tr>
+                                <tr>
+                                    <td>Màn hình</td>
+                                    <td>15.6 inch, 1920 x 1080 Pixels, IPS, 144 Hz, Anti-glare LED-backlit
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>CPU</td>
+                                    <td>Intel, Core i5, 10300H</td>
+                                </tr>
+                                <tr>
+                                    <td>RAM</td>
+                                    <td>8 GB (1 thanh 8 GB), DDR4, 2933 MHz</td>
+                                </tr>
+                                <tr>
+                                    <td>Ổ cứng</td>
+                                    <td>SSD 512 GB</td>
+                                </tr>
+                                <tr>
+                                    <td>Đồ họa</td>
+                                    <td>NVIDIA GeForce GTX 1650 4GB; Intel UHD Graphics</td>
+                                </tr>
+                                <tr>
+                                    <td>Hệ điều hành</td>
+                                    <td>Win 11</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <button type="button" class="row-sm-2 btn-danger btn-fill btn btn-lg btn-block"
+                        @click="notifyVue('top', 'right')">Mua
+                        hàng</button>
+                </div>
+                <div class="col-6">
+                    <!-- <h5 class="card-title" style="font-weight: bold">Giá sản phẩm:</h5> -->
+                    <div style="
+                          font-weight: bold;
+                          font-size: 50px;
+                          color: #363636;
+                          line-height: 1.25;
+                          /* margin-top: 10px; */
+                        ">Giá: 
+                        <span style="color:#FF4A55">49.000.000 đ</span>
+                    </div>
+                </div>
+            </div>
+            <!-- Mô tả sản phẩm -->
+            <div class="row">
+                <p class="text-justify">
+                    Hiệu năng cực đỉnh cho mọi tác vụ
+                    Hiệu năng đáng tin cậy trên ASUS TUF Gaming F15 FX506LH HN188W giúp bạn chơi game, livestream và
+                    thực hiện đa tác vụ.
+                    CPU Intel i5 tốc độ cao có thể kích hoạt nhiều luồng để xử lý đa nhiệm.
+                    Kết hợp với card màn hình GTX 1650, đáp ứng tốc độ khung hình cao của nhiều tựa game phổ biến.
+                    Tăng tốc độ tải ứng dụng với ổ SSD 512GB NVMe PCIe® và dễ dàng nâng cấp khả năng lưu trữ với ổ SSD
+                    thứ hai.
+                </p>
+            </div>
+            <!-- Câu hỏi thường gặp -->
+            <div class="row">
+
+
             </div>
 
         </div>
@@ -135,9 +118,7 @@
 </template>
 <script>
 export default {
-    components: {
-
-    },
+    components: {},
     data() {
         return {
             type: ['', 'info', 'success', 'warning', 'danger'],
@@ -151,8 +132,8 @@ export default {
             const color = Math.floor((Math.random() * 4) + 1)
             this.$notifications.notify(
                 {
-                    message: `<span>Thanh toán thành công !! <b>LaptopDD</b> - xin cảm ơn quý khách.</span>`,
-                    icon: 'nc-icon nc-send',
+                    message: `<span>Mua hàng thành công !!</span>`,
+                    icon: 'nc-icon nc-app',
                     horizontalAlign: horizontalAlign,
                     verticalAlign: verticalAlign,
                     type: this.type[2]
